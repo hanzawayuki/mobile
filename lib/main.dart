1c05414a3ee6de375ffa01fd632d530c6fcbb6c0
hanzawa <zeal20410115@fsgcl.jp>
2023-06-08 14:12:39 +0900

    履歴のページ作った

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,12 @@ class MoneyManagerApp extends StatelessWidget {
     return MaterialApp(
       title: 'Money Manager',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
       routes: {
         '/': (context) => TopPage(),
         '/moneyManager': (context) => HomePage(),
+        '/history': (context) => HistoryPage(),
       },
       initialRoute: '/',
     );
@@ -29,24 +30,43 @@ class TopPage extends StatelessWidget {
         title: Text('Top Page'),
       ),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pushNamed(context, '/moneyManager');
-          },
-          style: ElevatedButton.styleFrom(
-            padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
-            textStyle: TextStyle(fontSize: 24.0),
-            primary: Colors.red,
-          ),
-          child: Text('Open Money Manager'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '金銭管理アプリ',
+              style: TextStyle(
+                fontSize: 75.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 30),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/moneyManager');
+              },
+              style: ElevatedButton.styleFrom(
+                padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
+                textStyle: TextStyle(fontSize: 60.0),
+                primary: Colors.green,
+              ),
+              child: Text('スタート！'),
+            ),
+          ],
         ),
       ),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   TextEditingController amountController = TextEditingController();
+  List<String> history = [];
 
   void saveAmount(BuildContext context) {
     String amount = amountController.text;
@@ -54,6 +74,10 @@ class HomePage extends StatelessWidget {
 
     // 保存が完了したら、テキストフィールドをクリアする
     amountController.clear();
+
+    setState(() {
+      history.add(amount);
+    });
   }
 
   @override
@@ -77,9 +101,45 @@ class HomePage extends StatelessWidget {
             SizedBox(height: 16.0),
             ElevatedButton(
               onPressed: () => saveAmount(context),
+              style: ElevatedButton.styleFrom(
+                padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
+                textStyle: TextStyle(fontSize: 35.0),
+                primary: Colors.green,
+              ),
               child: Text('保存'),
             ),
+            SizedBox(height: 35.0),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/history');
+              },
+              style: ElevatedButton.styleFrom(
+                padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
+                textStyle: TextStyle(fontSize: 35.0),
+                primary: Colors.green,
+              ),
+              child: Text('金額履歴を表示'),
+            ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class HistoryPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('入力履歴ページ'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('戻る'),
         ),
       ),
     );
